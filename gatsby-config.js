882730125ce9e4c038f08c537a5bb04d1f56d8a2
Vinality <vibracre@gmail.com>
2020-02-13 09:32:04 -0300

    Trying to fix image pathing

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,8 +70,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Yakult Blog`,
+        short_name: `YaBlog`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
